6467b1cad761276ef6b2a28c70bb88220002fbbe
immutable-dev <immutable-org@protonmail.com>
2023-10-09 13:56:02 -0400

    entity type

--- src/gameObjects.ts
+++ src/gameObjects.ts
@@ -1,18 +1,22 @@
 import { Mesh, Vector3 } from 'three';
 import { GunOption } from './userStats.js';
+
+export type Entity = {
+  id: string;
+  position: [number, number, number];
+  rotation: [number, number, number];
+  keysPressed: Record<string, boolean>;
+  dead: boolean;
+}
+
 export type Player = {
-    id: string;
     gun: GunOption;
-    position: [number, number, number];
-    rotation: [number, number, number];
-    keysPressed: Record<string, boolean>;
     cameraRotation: [number, number, number];
     model: { laserHitbox: Mesh; collisionHitbox: Mesh };
-    dead: boolean;
     team?: 'green' | 'blue';
     authId?: string;
     username?: string;
-};
+} & Entity;
   
 export const ObjectTypesEnum = ['Player', 'PowerUp', 'Laser', 'Wall', 'Floor', 'Ladder', 'Tree1', 'Tree2', 'Rock', 'Stump', 'Grass', 'Zombie'] as const;
 
