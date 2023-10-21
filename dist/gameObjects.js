4eefd0977cc43a4699194c3e24ba562330595ee5
immutable-dev <immutable-org@protonmail.com>
2023-10-20 23:43:39 -0400

    yarn install

--- dist/gameObjects.js
+++ dist/gameObjects.js
@@ -1,2 +1,7 @@
-export const ObjectTypesEnum = ['Player', 'PowerUp', 'Laser', 'Wall', 'Floor', 'Ladder', 'Tree1', 'Tree2', 'Rock', 'Stump', 'Grass', 'Zombie', 'Agent Waypoint'];
+export const ObjectTypesEnum = ['Player',
+    'PowerUp', 'Laser', 'Wall', 'Floor', 'Ladder',
+    'Tree1', 'Tree2', 'Rock', 'Stump', 'Grass',
+    'Zombie', 'Agent Waypoint',
+    'Car01', 'Car02', 'Car03', 'Car04', 'Car05', 'Car06', 'Car07', 'Car8'
+];
 export const EntityTypesSet = ['Zombie'];
--- src/gameObjects.ts
+++ src/gameObjects.ts
@@ -25,7 +25,12 @@ export type Player = {
     username?: string;
 } & Entity;
   
-export const ObjectTypesEnum = ['Player', 'PowerUp', 'Laser', 'Wall', 'Floor', 'Ladder', 'Tree1', 'Tree2', 'Rock', 'Stump', 'Grass', 'Zombie', 'Agent Waypoint'] as const;
+export const ObjectTypesEnum = ['Player', 
+'PowerUp', 'Laser', 'Wall', 'Floor', 'Ladder',
+ 'Tree1', 'Tree2', 'Rock', 'Stump', 'Grass', 
+ 'Zombie', 'Agent Waypoint',
+'Car01','Car02','Car03','Car04','Car05','Car06','Car07','Car8'
+] as const;
 
 export type ObjectTypes = (typeof ObjectTypesEnum)[number];
 
