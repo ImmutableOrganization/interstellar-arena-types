fe5aee8dfa7ea3ad21febcce32f63f8f122971c9
immutable-dev <immutable-org@protonmail.com>
2023-10-22 18:03:30 -0400

    update types

--- dist/gameObjects.js
+++ dist/gameObjects.js
@@ -2,6 +2,7 @@ export const ObjectTypesEnum = ['Player',
     'PowerUp', 'Laser', 'Wall', 'Floor', 'Ladder',
     'Tree1', 'Tree2', 'Rock', 'Stump', 'Grass',
     'Zombie', 'Agent Waypoint',
-    'Car01', 'Car02', 'Car03', 'Car04', 'Car05', 'Car06', 'Car07', 'Car08'
+    'Car01', 'Car02', 'Car03', 'Car04', 'Car05', 'Car06', 'Car07', 'Car08',
+    'Circuit_Breaker', 'Generator', 'Pipe_Valve', 'Pump', 'Switch_1', 'Switch_2', 'Switch_3', 'Transformer', 'Turbine'
 ];
 export const EntityTypesSet = ['Zombie'];
