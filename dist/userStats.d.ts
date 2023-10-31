2d39ecdc4d7b2d616a13aa23baa9cecb0e37e942
immutable-dev <immutable-org@protonmail.com>
2023-10-30 21:31:14 -0400

    updaet types

--- dist/userStats.d.ts
+++ dist/userStats.d.ts
@@ -1,4 +1,4 @@
-export declare const _gunOptions: readonly ["Rifle", "Shotgun", "SMG", "Sniper"];
+export declare const _gunOptions: readonly ["AssaultRifle_1", "AssaultRifle_2", "AssaultRifle_3", "AssaultRifle_4", "AssaultRifle_5", "AssaultRifle2_1", "AssaultRifle2_2", "AssaultRifle2_3", "AssaultRifle2_4", "Bullpup_1", "Bullpup_2", "Pistol_1", "Pistol_2", "Pistol_3", "Pistol_4", "Pistol_5", "Pistol_6", "Revolver_1", "Revolver_2", "Revolver_3", "Revolver_4", "Revolver_5", "Shotgun_1", "Shotgun_2", "Shotgun_3", "Shotgun_4", "Shotgun_SawedOff", "Shotgun_ShortStock", "SniperRifle_1", "SniperRifle_2", "SniperRifle_3", "SniperRifle_4", "SniperRifle_5", "SniperRifle_6", "SubmachineGun_1", "SubmachineGun_2", "SubmachineGun_3", "SubmachineGun_4", "SubmachineGun_5"];
 export type GunOption = (typeof _gunOptions)[number];
 export type UserStatsType = {
     username: string;
--- dist/userStats.js
+++ dist/userStats.js
@@ -1,27 +1,277 @@
-export const _gunOptions = ['Rifle', 'Shotgun', 'SMG', 'Sniper'];
+export const _gunOptions = [
+    "AssaultRifle_1",
+    "AssaultRifle_2",
+    "AssaultRifle_3",
+    "AssaultRifle_4",
+    "AssaultRifle_5",
+    "AssaultRifle2_1",
+    "AssaultRifle2_2",
+    "AssaultRifle2_3",
+    "AssaultRifle2_4",
+    "Bullpup_1",
+    "Bullpup_2",
+    "Pistol_1",
+    "Pistol_2",
+    "Pistol_3",
+    "Pistol_4",
+    "Pistol_5",
+    "Pistol_6",
+    "Revolver_1",
+    "Revolver_2",
+    "Revolver_3",
+    "Revolver_4",
+    "Revolver_5",
+    "Shotgun_1",
+    "Shotgun_2",
+    "Shotgun_3",
+    "Shotgun_4",
+    "Shotgun_SawedOff",
+    "Shotgun_ShortStock",
+    "SniperRifle_1",
+    "SniperRifle_2",
+    "SniperRifle_3",
+    "SniperRifle_4",
+    "SniperRifle_5",
+    "SniperRifle_6",
+    "SubmachineGun_1",
+    "SubmachineGun_2",
+    "SubmachineGun_3",
+    "SubmachineGun_4",
+    "SubmachineGun_5"
+];
 export const GunConfig = {
-    Rifle: {
+    AssaultRifle_1: {
         damage: 30,
         fireRate: 100,
         range: 1000,
-        ammoCapacity: 30,
+        ammoCapacity: 30
     },
-    SMG: {
-        damage: 20,
-        fireRate: 50,
+    AssaultRifle_2: {
+        damage: 30,
+        fireRate: 100,
         range: 1000,
-        ammoCapacity: 60,
+        ammoCapacity: 30
     },
-    Shotgun: {
+    AssaultRifle_3: {
+        damage: 30,
+        fireRate: 100,
+        range: 1000,
+        ammoCapacity: 30
+    },
+    AssaultRifle_4: {
+        damage: 30,
+        fireRate: 100,
+        range: 1000,
+        ammoCapacity: 30
+    },
+    AssaultRifle_5: {
+        damage: 30,
+        fireRate: 100,
+        range: 1000,
+        ammoCapacity: 30
+    },
+    AssaultRifle2_1: {
+        damage: 30,
+        fireRate: 100,
+        range: 1000,
+        ammoCapacity: 30
+    },
+    AssaultRifle2_2: {
+        damage: 30,
+        fireRate: 100,
+        range: 1000,
+        ammoCapacity: 30
+    },
+    AssaultRifle2_3: {
+        damage: 30,
+        fireRate: 100,
+        range: 1000,
+        ammoCapacity: 30
+    },
+    AssaultRifle2_4: {
+        damage: 30,
+        fireRate: 100,
+        range: 1000,
+        ammoCapacity: 30
+    },
+    Bullpup_1: {
+        damage: 30,
+        fireRate: 100,
+        range: 1000,
+        ammoCapacity: 30
+    },
+    Bullpup_2: {
+        damage: 30,
+        fireRate: 100,
+        range: 1000,
+        ammoCapacity: 30
+    },
+    Pistol_1: {
         damage: 15,
         fireRate: 300,
         range: 600,
-        ammoCapacity: 8,
+        ammoCapacity: 8 // Pistol is considered as a Shotgun
     },
-    Sniper: {
+    Pistol_2: {
+        damage: 15,
+        fireRate: 300,
+        range: 600,
+        ammoCapacity: 8 // Pistol is considered as a Shotgun
+    },
+    Pistol_3: {
+        damage: 15,
+        fireRate: 300,
+        range: 600,
+        ammoCapacity: 8 // Pistol is considered as a Shotgun
+    },
+    Pistol_4: {
+        damage: 15,
+        fireRate: 300,
+        range: 600,
+        ammoCapacity: 8 // Pistol is considered as a Shotgun
+    },
+    Pistol_5: {
+        damage: 15,
+        fireRate: 300,
+        range: 600,
+        ammoCapacity: 8 // Pistol is considered as a Shotgun
+    },
+    Pistol_6: {
+        damage: 15,
+        fireRate: 300,
+        range: 600,
+        ammoCapacity: 8 // Pistol is considered as a Shotgun
+    },
+    Revolver_1: {
+        damage: 30,
+        fireRate: 100,
+        range: 1000,
+        ammoCapacity: 30
+    },
+    Revolver_2: {
+        damage: 30,
+        fireRate: 100,
+        range: 1000,
+        ammoCapacity: 30
+    },
+    Revolver_3: {
+        damage: 30,
+        fireRate: 100,
+        range: 1000,
+        ammoCapacity: 30
+    },
+    Revolver_4: {
+        damage: 30,
+        fireRate: 100,
+        range: 1000,
+        ammoCapacity: 30
+    },
+    Revolver_5: {
+        damage: 30,
+        fireRate: 100,
+        range: 1000,
+        ammoCapacity: 30
+    },
+    Shotgun_1: {
+        damage: 15,
+        fireRate: 300,
+        range: 600,
+        ammoCapacity: 8
+    },
+    Shotgun_2: {
+        damage: 15,
+        fireRate: 300,
+        range: 600,
+        ammoCapacity: 8
+    },
+    Shotgun_3: {
+        damage: 15,
+        fireRate: 300,
+        range: 600,
+        ammoCapacity: 8
+    },
+    Shotgun_4: {
+        damage: 15,
+        fireRate: 300,
+        range: 600,
+        ammoCapacity: 8
+    },
+    Shotgun_SawedOff: {
+        damage: 15,
+        fireRate: 300,
+        range: 600,
+        ammoCapacity: 8
+    },
+    Shotgun_ShortStock: {
+        damage: 15,
+        fireRate: 300,
+        range: 600,
+        ammoCapacity: 8
+    },
+    SniperRifle_1: {
         damage: 100,
         fireRate: 500,
         range: 5000,
-        ammoCapacity: 10,
+        ammoCapacity: 10
     },
+    SniperRifle_2: {
+        damage: 100,
+        fireRate: 500,
+        range: 5000,
+        ammoCapacity: 10
+    },
+    SniperRifle_3: {
+        damage: 100,
+        fireRate: 500,
+        range: 5000,
+        ammoCapacity: 10
+    },
+    SniperRifle_4: {
+        damage: 100,
+        fireRate: 500,
+        range: 5000,
+        ammoCapacity: 10
+    },
+    SniperRifle_5: {
+        damage: 100,
+        fireRate: 500,
+        range: 5000,
+        ammoCapacity: 10
+    },
+    SniperRifle_6: {
+        damage: 100,
+        fireRate: 500,
+        range: 5000,
+        ammoCapacity: 10
+    },
+    SubmachineGun_1: {
+        damage: 20,
+        fireRate: 50,
+        range: 1000,
+        ammoCapacity: 60
+    },
+    SubmachineGun_2: {
+        damage: 20,
+        fireRate: 50,
+        range: 1000,
+        ammoCapacity: 60
+    },
+    SubmachineGun_3: {
+        damage: 20,
+        fireRate: 50,
+        range: 1000,
+        ammoCapacity: 60
+    },
+    SubmachineGun_4: {
+        damage: 20,
+        fireRate: 50,
+        range: 1000,
+        ammoCapacity: 60
+    },
+    SubmachineGun_5: {
+        damage: 20,
+        fireRate: 50,
+        range: 1000,
+        ammoCapacity: 60
+    }
 };
