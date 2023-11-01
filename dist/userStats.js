cf3273a597bfb988bb531f3b513eb7be48499c20
immutable-dev <immutable-org@protonmail.com>
2023-11-01 14:58:15 -0400

    update gunrange

--- dist/userStats.js
+++ dist/userStats.js
@@ -43,235 +43,235 @@ export const GunConfig = {
     AssaultRifle_1: {
         damage: 30,
         fireRate: 100,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 30
     },
     AssaultRifle_2: {
         damage: 30,
         fireRate: 100,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 30
     },
     AssaultRifle_3: {
         damage: 30,
         fireRate: 100,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 30
     },
     AssaultRifle_4: {
         damage: 30,
         fireRate: 100,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 30
     },
     AssaultRifle_5: {
         damage: 30,
         fireRate: 100,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 30
     },
     AssaultRifle2_1: {
         damage: 30,
         fireRate: 100,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 30
     },
     AssaultRifle2_2: {
         damage: 30,
         fireRate: 100,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 30
     },
     AssaultRifle2_3: {
         damage: 30,
         fireRate: 100,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 30
     },
     AssaultRifle2_4: {
         damage: 30,
         fireRate: 100,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 30
     },
     Bullpup_1: {
         damage: 30,
         fireRate: 100,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 30
     },
     Bullpup_2: {
         damage: 30,
         fireRate: 100,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 30
     },
     Pistol_1: {
         damage: 15,
         fireRate: 300,
-        range: 600,
+        range: 2500,
         ammoCapacity: 8 // Pistol is considered as a Shotgun
     },
     Pistol_2: {
         damage: 15,
         fireRate: 300,
-        range: 600,
+        range: 2500,
         ammoCapacity: 8 // Pistol is considered as a Shotgun
     },
     Pistol_3: {
         damage: 15,
         fireRate: 300,
-        range: 600,
+        range: 2500,
         ammoCapacity: 8 // Pistol is considered as a Shotgun
     },
     Pistol_4: {
         damage: 15,
         fireRate: 300,
-        range: 600,
+        range: 2500,
         ammoCapacity: 8 // Pistol is considered as a Shotgun
     },
     Pistol_5: {
         damage: 15,
         fireRate: 300,
-        range: 600,
+        range: 2500,
         ammoCapacity: 8 // Pistol is considered as a Shotgun
     },
     Pistol_6: {
         damage: 15,
         fireRate: 300,
-        range: 600,
+        range: 2500,
         ammoCapacity: 8 // Pistol is considered as a Shotgun
     },
     Revolver_1: {
         damage: 30,
         fireRate: 100,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 30
     },
     Revolver_2: {
         damage: 30,
         fireRate: 100,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 30
     },
     Revolver_3: {
         damage: 30,
         fireRate: 100,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 30
     },
     Revolver_4: {
         damage: 30,
         fireRate: 100,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 30
     },
     Revolver_5: {
         damage: 30,
         fireRate: 100,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 30
     },
     Shotgun_1: {
         damage: 15,
         fireRate: 300,
-        range: 600,
+        range: 2500,
         ammoCapacity: 8
     },
     Shotgun_2: {
         damage: 15,
         fireRate: 300,
-        range: 600,
+        range: 2500,
         ammoCapacity: 8
     },
     Shotgun_3: {
         damage: 15,
         fireRate: 300,
-        range: 600,
+        range: 2500,
         ammoCapacity: 8
     },
     Shotgun_4: {
         damage: 15,
         fireRate: 300,
-        range: 600,
+        range: 2500,
         ammoCapacity: 8
     },
     Shotgun_SawedOff: {
         damage: 15,
         fireRate: 300,
-        range: 600,
+        range: 2500,
         ammoCapacity: 8
     },
     Shotgun_ShortStock: {
         damage: 15,
         fireRate: 300,
-        range: 600,
+        range: 2500,
         ammoCapacity: 8
     },
     SniperRifle_1: {
         damage: 100,
         fireRate: 500,
-        range: 5000,
+        range: 10000,
         ammoCapacity: 10
     },
     SniperRifle_2: {
         damage: 100,
         fireRate: 500,
-        range: 5000,
+        range: 10000,
         ammoCapacity: 10
     },
     SniperRifle_3: {
         damage: 100,
         fireRate: 500,
-        range: 5000,
+        range: 10000,
         ammoCapacity: 10
     },
     SniperRifle_4: {
         damage: 100,
         fireRate: 500,
-        range: 5000,
+        range: 10000,
         ammoCapacity: 10
     },
     SniperRifle_5: {
         damage: 100,
         fireRate: 500,
-        range: 5000,
+        range: 10000,
         ammoCapacity: 10
     },
     SniperRifle_6: {
         damage: 100,
         fireRate: 500,
-        range: 5000,
+        range: 10000,
         ammoCapacity: 10
     },
     SubmachineGun_1: {
         damage: 20,
         fireRate: 50,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 60
     },
     SubmachineGun_2: {
         damage: 20,
         fireRate: 50,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 60
     },
     SubmachineGun_3: {
         damage: 20,
         fireRate: 50,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 60
     },
     SubmachineGun_4: {
         damage: 20,
         fireRate: 50,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 60
     },
     SubmachineGun_5: {
         damage: 20,
         fireRate: 50,
-        range: 1000,
+        range: 3000,
         ammoCapacity: 60
     }
 };
