c0678bb0286ee15b280f84216e35ab081e395014
immutable-dev <immutable-org@protonmail.com>
2024-03-10 16:55:06 -0400

    transparent texture

--- src/map.ts
+++ src/map.ts
@@ -3,7 +3,7 @@ import { GunOption } from "userStats.js";
 
 export const colors = ['#ff00ff', '#4A1D96', '#5521B5', '#6C2BD9', '#7E3AF2', '#363838'];
 export const floorColors = ['#282828', '#696969'];
-export const textureUrls = ['floor-checker', 'floor-brick', 'floor-dirt', 'floor-metal', 'floor-stone', 'floor-tile', 'floor-wood', 'road', 'fence', 'concreteBlock'] as const;
+export const textureUrls = ['floor-checker', 'floor-brick', 'floor-dirt', 'floor-metal', 'floor-stone', 'floor-tile', 'floor-wood', 'road', 'fence', 'concreteBlock', 'transparent'] as const;
 export type BuyableItem = {
   cost: number;
   item: GunOption;
